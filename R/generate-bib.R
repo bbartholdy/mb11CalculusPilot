7b13299de40f6a63ad7d6abf69574777d86102f0
bbartholdy <47455812+bbartholdy@users.noreply.github.com>
2023-02-17 13:52:06 +0100

    add function to generate bibliography

--- R/generate-bib.R
+++ R/generate-bib.R
@@ -4,10 +4,10 @@
 #' @importFrom here here
 
 generate_bib <- function(){
-  cite_keys <- bbt_detect_citations(list.files(here("inst/paper/"), "*.qmd", full.names = T))
+  cite_keys <- bbt_detect_citations(list.files(here("analysis/paper/"), "*.qmd", full.names = T))
   ignore_keys <- grep("fig-|tbl-", cite_keys, value = T)
   bbt_write_bib(
-    here("inst/paper/references.bib"),
+    here("analysis/paper/references.bib"),
     keys = cite_keys,
     ignore = ignore_keys,
     overwrite = T
